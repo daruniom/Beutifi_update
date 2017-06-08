2a99961e747217364c8e0a775f852ce92b206021
Miguel y Alberto <miguelbogav@gmail.com>
2017-06-08 12:48:23 +0100

    Add project files.

---
+++ Beutifi_update/frmUpdate.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUpdate
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblCopy = New System.Windows.Forms.Label()
+        Me.dtpCopy = New System.Windows.Forms.DateTimePicker()
+        Me.dtpReplication = New System.Windows.Forms.DateTimePicker()
+        Me.lblReplication = New System.Windows.Forms.Label()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.lblServer = New System.Windows.Forms.Label()
+        Me.txtServer = New System.Windows.Forms.TextBox()
+        Me.txtDatabase = New System.Windows.Forms.TextBox()
+        Me.lblDatabase = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.lblUser = New System.Windows.Forms.Label()
+        Me.lblConnected = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblCopy
+        '
+        Me.lblCopy.AutoSize = True
+        Me.lblCopy.Location = New System.Drawing.Point(329, 70)
+        Me.lblCopy.Name = "lblCopy"
+        Me.lblCopy.Size = New System.Drawing.Size(58, 13)
+        Me.lblCopy.TabIndex = 0
+        Me.lblCopy.Text = "Copy date:"
+        Me.lblCopy.Visible = False
+        '
+        'dtpCopy
+        '
+        Me.dtpCopy.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpCopy.Location = New System.Drawing.Point(436, 67)
+        Me.dtpCopy.MaxDate = New Date(2025, 12, 31, 0, 0, 0, 0)
+        Me.dtpCopy.MinDate = New Date(2016, 1, 1, 0, 0, 0, 0)
+        Me.dtpCopy.Name = "dtpCopy"
+        Me.dtpCopy.Size = New System.Drawing.Size(97, 20)
+        Me.dtpCopy.TabIndex = 1
+        Me.dtpCopy.Visible = False
+        '
+        'dtpReplication
+        '
+        Me.dtpReplication.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpReplication.Location = New System.Drawing.Point(436, 107)
+        Me.dtpReplication.MaxDate = New Date(2025, 12, 31, 0, 0, 0, 0)
+        Me.dtpReplication.MinDate = New Date(2016, 1, 1, 0, 0, 0, 0)
+        Me.dtpReplication.Name = "dtpReplication"
+        Me.dtpReplication.Size = New System.Drawing.Size(97, 20)
+        Me.dtpReplication.TabIndex = 3
+        Me.dtpReplication.Visible = False
+        '
+        'lblReplication
+        '
+        Me.lblReplication.AutoSize = True
+        Me.lblReplication.Location = New System.Drawing.Point(329, 110)
+        Me.lblReplication.Name = "lblReplication"
+        Me.lblReplication.Size = New System.Drawing.Size(87, 13)
+        Me.lblReplication.TabIndex = 2
+        Me.lblReplication.Text = "Replication date:"
+        Me.lblReplication.Visible = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(397, 147)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 4
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        Me.btnUpdate.Visible = False
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(122, 190)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(75, 23)
+        Me.btnCheck.TabIndex = 5
+        Me.btnCheck.Text = "Check"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'lblServer
+        '
+        Me.lblServer.AutoSize = True
+        Me.lblServer.Location = New System.Drawing.Point(30, 30)
+        Me.lblServer.Name = "lblServer"
+        Me.lblServer.Size = New System.Drawing.Size(38, 13)
+        Me.lblServer.TabIndex = 6
+        Me.lblServer.Text = "Server"
+        '
+        'txtServer
+        '
+        Me.txtServer.Location = New System.Drawing.Point(106, 27)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(173, 20)
+        Me.txtServer.TabIndex = 7
+        '
+        'txtDatabase
+        '
+        Me.txtDatabase.Location = New System.Drawing.Point(106, 67)
+        Me.txtDatabase.Name = "txtDatabase"
+        Me.txtDatabase.Size = New System.Drawing.Size(173, 20)
+        Me.txtDatabase.TabIndex = 9
+        '
+        'lblDatabase
+        '
+        Me.lblDatabase.AutoSize = True
+        Me.lblDatabase.Location = New System.Drawing.Point(30, 70)
+        Me.lblDatabase.Name = "lblDatabase"
+        Me.lblDatabase.Size = New System.Drawing.Size(56, 13)
+        Me.lblDatabase.TabIndex = 8
+        Me.lblDatabase.Text = "Database:"
+        '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(106, 107)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(173, 20)
+        Me.txtUser.TabIndex = 11
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(106, 147)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(173, 20)
+        Me.txtPassword.TabIndex = 13
+        Me.txtPassword.UseSystemPasswordChar = True
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(30, 150)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(56, 13)
+        Me.lblPassword.TabIndex = 12
+        Me.lblPassword.Text = "Password:"
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Location = New System.Drawing.Point(30, 110)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(61, 13)
+        Me.lblUser.TabIndex = 10
+        Me.lblUser.Text = "User name:"
+        '
+        'lblConnected
+        '
+        Me.lblConnected.AutoSize = True
+        Me.lblConnected.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConnected.Location = New System.Drawing.Point(386, 27)
+        Me.lblConnected.Name = "lblConnected"
+        Me.lblConnected.Size = New System.Drawing.Size(96, 20)
+        Me.lblConnected.TabIndex = 14
+        Me.lblConnected.Text = "Connected"
+        Me.lblConnected.Visible = False
+        '
+        'frmUpdate
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(558, 233)
+        Me.Controls.Add(Me.lblConnected)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.lblUser)
+        Me.Controls.Add(Me.txtDatabase)
+        Me.Controls.Add(Me.lblDatabase)
+        Me.Controls.Add(Me.txtServer)
+        Me.Controls.Add(Me.lblServer)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.dtpReplication)
+        Me.Controls.Add(Me.lblReplication)
+        Me.Controls.Add(Me.dtpCopy)
+        Me.Controls.Add(Me.lblCopy)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmUpdate"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Update availability"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblCopy As Label
+    Friend WithEvents dtpCopy As DateTimePicker
+    Friend WithEvents dtpReplication As DateTimePicker
+    Friend WithEvents lblReplication As Label
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnCheck As Button
+    Friend WithEvents lblServer As Label
+    Friend WithEvents txtServer As TextBox
+    Friend WithEvents txtDatabase As TextBox
+    Friend WithEvents lblDatabase As Label
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents lblUser As Label
+    Friend WithEvents lblConnected As Label
+End Class
---
+++ Beutifi_update/frmUpdate.vb
@@ -0,0 +1,167 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmUpdate
+
+    Dim stringConnection As String  'Variable to store the connection string with all de parameters typed by the user
+
+    Private Sub btnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        If btnCheck.Text = "Check" Then     'Form fill-up check
+            If txtServer.Text = String.Empty Then
+                EmptyNotice()
+                txtServer.Focus()
+            ElseIf txtDatabase.Text = String.Empty Then
+                EmptyNotice()
+                txtDatabase.Focus()
+            ElseIf txtUser.Text = String.Empty Then
+                EmptyNotice()
+                txtUser.Focus()
+            ElseIf txtPassword.Text = String.Empty Then
+                EmptyNotice()
+                txtPassword.Focus()
+            Else    'Performs connection data check by trying to create and open a connection
+
+                'Remote connection string: 
+                stringConnection = "Data Source=" & txtServer.Text & ";Initial Catalog=" & txtDatabase.Text &
+                   ";User ID=" & txtUser.Text & ";Password=" & txtPassword.Text & ";Connection Timeout=50;"
+
+                Try
+                    Dim sqlConnection1 As New System.Data.SqlClient.SqlConnection(stringConnection)
+                    sqlConnection1.Open()
+                    sqlConnection1.Close()
+                    DisableConnection()
+                    'User basic guidelines
+                    MsgBox("Copy date is a date with a correct availability." & vbCrLf &
+                   "Replication date is the date from when availability will be filled." & vbCrLf &
+                   "Both dates should be the same day of the week.", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Connection established")
+                    ShowUpdate()
+                Catch ex As Exception
+                    MsgBox("Connection error. Review data and try again." & vbCrLf & vbCrLf & ex.Message,
+                                    MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Error")
+                End Try
+            End If
+        Else    'Allows change connection data
+            HideUpdate()
+            UpdateReset()
+            EnableConnection()
+        End If
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If Not dtpCopy.Value.DayOfWeek = dtpReplication.Value.DayOfWeek Then    'Same day of week check
+            MsgBox("Different day of week selected in both date fields", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Warning!")
+        Else
+            'Operation variables
+            Dim Interval As Integer = DateDiff(DateInterval.Day, dtpCopy.Value, dtpReplication.Value)
+            Dim week As Date = DateAdd(DateInterval.Day, 6, dtpCopy.Value)
+
+            Dim stringSelect As String = "SELECT StaffID, StartDate, EndDate FROM Schedule WHERE StartDate BETWEEN '" _
+                & dtpCopy.Value.ToString("yyyy/MM/dd") & "' AND '" & week.ToString("yyyy/MM/dd") & "' ORDER BY StartDate"
+            Dim stringInsert As String
+
+            'Execution variables
+            Dim command As SqlCommand
+            Dim da As SqlDataAdapter
+            Dim ds As New DataSet
+            Dim dsCheck As New DataSet
+
+            Try
+                Dim connection As New SqlClient.SqlConnection
+                connection.ConnectionString = stringConnection
+                command = New SqlCommand(stringSelect, connection)
+                command.CommandTimeout = 3000
+                da = New SqlDataAdapter(command)
+                da.Fill(ds, "Schedule")     'Fetching original data to replicate into a data set
+                Dim rowNumber As Integer = ds.Tables("Schedule").Rows.Count
+                If rowNumber > 0 Then
+                    Dim fila As DataRow
+                    'Remote server autogenerates primary key, but local testing copy doesn't, so we needed PK variable in development environment
+                    'Dim PK As Integer = 2000000
+
+                    'Variables to use in the insert string
+                    Dim staff As Integer
+                    Dim startAvailability As Date
+                    Dim endAvailability As Date
+
+                    Dim count As Integer = 0    'counter to save the rows added during the update
+
+                    For i = 0 To rowNumber - 1      'Rows start at 0
+                        fila = ds.Tables("Schedule").Rows(i)
+                        For j = 0 To 13     'Replicates data 14 weeks
+                            staff = fila("StaffID")
+
+                            'Variables used to preserve the original timetable
+                            startAvailability = DateAdd(DateInterval.Day, Interval + (7 * j), fila("StartDate"))
+                            endAvailability = DateAdd(DateInterval.Day, Interval + (7 * j), fila("EndDate"))
+
+                            stringInsert = "INSERT Schedule (ScheduleID, StaffID, StartDate, EndDate, CreatedOn, UpdatedOn) VALUES (" _
+                                & staff & ", '" & startAvailability & "', '" & endAvailability & "', '" & Today & "', '" & Today & "')"
+
+                            Try
+                                Dim sqlConnection1 As New System.Data.SqlClient.SqlConnection(stringConnection)
+                                Dim cmd = New System.Data.SqlClient.SqlCommand
+                                cmd.CommandType = System.Data.CommandType.Text
+                                cmd.CommandText = stringInsert
+                                cmd.Connection = sqlConnection1
+
+                                sqlConnection1.Open()
+                                cmd.ExecuteNonQuery()
+                                count += 1
+                                sqlConnection1.Close()
+
+                            Catch ex As Exception
+                                MsgBox(ex.Message, MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Error")
+                            End Try
+                            'PK += 1
+                        Next
+                    Next
+                    MsgBox(count & " rows added", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "Update finished")
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Error")
+            End Try
+        End If
+    End Sub
+
+    Private Sub EmptyNotice()
+        MsgBox("All fields must be filled", MsgBoxStyle.Exclamation + MsgBoxStyle.OkOnly, "Warning!")
+    End Sub
+
+    Private Sub HideUpdate()    'Hides update section
+        lblConnected.Visible = False
+        lblCopy.Visible = False
+        dtpCopy.Visible = False
+        lblReplication.Visible = False
+        dtpReplication.Visible = False
+        btnUpdate.Visible = False
+    End Sub
+
+    Private Sub ShowUpdate()    'Shows update section
+        lblConnected.Visible = True
+        lblCopy.Visible = True
+        dtpCopy.Visible = True
+        lblReplication.Visible = True
+        dtpReplication.Visible = True
+        btnUpdate.Visible = True
+    End Sub
+
+    Private Sub DisableConnection()     'Blocks connection data controls to avoid changing values during 
+        txtServer.Enabled = False       'update setting or execution and changes button text property
+        txtDatabase.Enabled = False
+        txtUser.Enabled = False
+        txtPassword.Enabled = False
+        btnCheck.Text = "Back"
+    End Sub
+
+    Private Sub EnableConnection()      'Enables connection data controls and changes button text property
+        txtServer.Enabled = True
+        txtDatabase.Enabled = True
+        txtUser.Enabled = True
+        txtPassword.Enabled = True
+        btnCheck.Text = "Check"
+    End Sub
+
+    Private Sub UpdateReset()       'Restarts data field values
+        dtpCopy.ResetText()
+        dtpReplication.ResetText()
+    End Sub
+End Class
